09d2de33546e96ed1fda23dfe30186c3dc1518eb
Adrian Cosmin Nedelcu <cosminadrian1304@gmail.com>
2023-08-31 23:00:33 +0300

    task 1 - use subscribe (interval, map, filter)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BandListComponent } from './band-list/band-list.component';
+import {HttpClientModule} from "@angular/common/http";
+import {BandDataService} from "./band-data.service";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BandListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    BandDataService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
