3c0f70593619c1acba6c53af38dd7adad7b5b0ca
Adrian Cosmin Nedelcu <cosminadrian1304@gmail.com>
2023-08-31 23:21:58 +0300

    task 2 - use async

--- src/app/band-list/band-list.component.ts
+++ src/app/band-list/band-list.component.ts
@@ -2,29 +2,16 @@ import { Component } from '@angular/core';
 
 import { BandDataService } from '../band-data.service';
 import { Band } from '../model';
-import {filter, interval, map} from "rxjs";
+import {BehaviorSubject, filter, interval, map, Observable} from "rxjs";
 
 @Component({
   selector: 'app-band-list',
   templateUrl: 'band-list.component.html'
 })
 export class BandListComponent {
-  bandList: Band[] = [];
-
-  //observable-urile sunt chestii care ne vor fi date in viitor
-  //stream-uri de evenimente
+  bandList$: Observable<Band[]>;
 
   constructor(private bandDataService: BandDataService) {
-    const o = interval(1000).pipe(
-      map(value => value * value),
-      filter(value => value % 2 === 0)
-    )
-    o.subscribe(
-      (value) => console.log(value)
-    );
-
-    this.bandDataService.getBands().subscribe(bands => {
-      this.bandList = bands;
-    });
+    this.bandList$ = this.bandDataService.getBands();
   }
 }
