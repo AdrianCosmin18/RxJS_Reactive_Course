c31e6fde6d62e4258e28453dbebba0323cde43a3
Adrian Cosmin Nedelcu <cosminadrian1304@gmail.com>
2023-08-31 23:37:43 +0300

    task 3 - exemple of using subject

--- src/app/band-list/band-list.component.ts
+++ src/app/band-list/band-list.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 import { BandDataService } from '../band-data.service';
 import { Band } from '../model';
-import {BehaviorSubject, filter, interval, map, Observable} from "rxjs";
+import {BehaviorSubject, filter, interval, map, Observable, Subject} from "rxjs";
 
 @Component({
   selector: 'app-band-list',
@@ -11,7 +11,13 @@ import {BehaviorSubject, filter, interval, map, Observable} from "rxjs";
 export class BandListComponent {
   bandList$: Observable<Band[]>;
 
+  //subject-ul e un observable care poate fi controlat
+  refreshDataClickSubject = new Subject();
+
   constructor(private bandDataService: BandDataService) {
     this.bandList$ = this.bandDataService.getBands();
+
+    const refreshDataClick$= this.refreshDataClickSubject.asObservable();
+    refreshDataClick$.subscribe(() => console.log("click"))
   }
 }
